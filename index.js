9ef2fe8678cf34e4a3832f10b037edcb7160d07e
Yegeta <149103185+Yegetaa@users.noreply.github.com>
2024-01-18 11:18:38 -0500

    "files created"

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import "./loadEnv.js";
+import grades from "./routes/grades.js";
+
+const app = express();
+const PORT = process.env.PORT || 5050;
+
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the API.");
+});
+
+app.use("/grades", grades);
+
+// Global error handling
+app.use((err, _req, res, next) => {
+  res.status(500).send("Seems like we messed up somewhere...");
+});
+
+// Start the Express server
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
